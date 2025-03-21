98b4c560c3268e715fef79845eb0b8ed826abf94
underethsea <underethsea@gmail.com>
2025-03-21 14:27:05 -0700

    clear claimed rewards

--- utils/getUserRewards.ts
+++ utils/getUserRewards.ts
@@ -167,7 +167,7 @@ export async function GetUsersAwards(
         );
      
         metaCompletedCalls.push(
-          metaRewardsContract.callStatic.calculateRewards(
+          metaRewardsContract.callStatic.claimRewards(
             promo.vault,
             userAddress,
             promo.promotionId,
@@ -192,8 +192,9 @@ export async function GetUsersAwards(
 
   // Append meta rewards to completed with meta: true
   metaCompletedPromotions.forEach((promo: any, index: number) => {
-    const rewardArray = metaRewardsData[index];
-    const rewardAmount = rewardArray.reduce((sum, r) => sum.add(r), ethers.BigNumber.from(0));    
+    // const rewardArray = metaRewardsData[index];
+    // const rewardAmount = rewardArray.reduce((sum, r) => sum.add(r), ethers.BigNumber.from(0));    
+    const rewardAmount = metaRewardsData[index] as any;
     console.log("Reward amount",rewardAmount)
     if (rewardAmount.gt(0)) {
       userRewards.completed.push({
